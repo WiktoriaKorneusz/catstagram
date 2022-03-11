1c19e74f74490ffc216506e990f5433d5b5a8e53
Wiktoria Korneusz <wiktoriakorneusz@gmail.com>
2022-03-11 23:18:22 +0100

    fixed problem with images

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require("./data/database");
 const routes = require("./routes/routes");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 app.set("view engine", "ejs");
